aeec63bd3d4004f60a69782e0486268f02cd80e5
khavin krishnan <khavinkrishnan@gmail.com>
2021-04-12 21:11:47 +0530

    adding list and edit component

--- src/app/calendar/date-cell/date-cell.component.ts
+++ src/app/calendar/date-cell/date-cell.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { changeSelectedDate } from '../calendar.actions';
 
 @Component({
   selector: 'app-date-cell',
@@ -7,11 +9,16 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class DateCellComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store:Store) { }
 
-  @Input() date:number;
+  @Input() date:Object;
   
   ngOnInit(): void {
+  }
+
+  selectDate(): void {
+    console.log(this.date);
+    this.store.dispatch(changeSelectedDate({date: new Date(this.date["year"],this.date["month"],this.date["date"]+1)}))
   }
 
 }
--- src/app/notes/notes.actions.ts
+++ src/app/notes/notes.actions.ts
@@ -3,3 +3,4 @@ import { createAction,props } from '@ngrx/store';
 export const createNote = createAction("[Notes] Note Created",props<{ id: string, title?: string, content?: string, color: string, tags?: Array<string> }>());
 export const editNote = createAction("[Notes] Note Edited",props<{ id: string, title?: string, content?: string, color?: string, tags?: Array<string> }>());
 export const deleteNote = createAction("[Notes] Note Deleted",props<{ id: string }>());
+export const selectNote = createAction("[Notes] Note Selected",props<{ id: string }>());
