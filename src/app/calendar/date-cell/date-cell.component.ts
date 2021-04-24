27892d188c5000c360a538939f677fe784d20ff4
khavin krishnan <khavinkrishnan@khavins-MacBook-Air-2.local>
2021-04-24 19:48:09 +0530

    adding colors to calendar

--- src/app/calendar/date-cell/date-cell.component.ts
+++ src/app/calendar/date-cell/date-cell.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { changeSelectedDate } from '../calendar.actions';
+import { colors,colorDefinitions,allColors } from '../../colors/colors.constant';
 
 @Component({
   selector: 'app-date-cell',
@@ -12,17 +13,22 @@ export class DateCellComponent implements OnInit {
   constructor(private store:Store) { }
 
   @Input() date:Object;
-  
+  colorPalette:colors = colorDefinitions;
+
   ngOnInit(): void {
-    console.log(this.date);
+    console.log('here');
+  }
+
+  ngOnChanges(changes:any): void {
+    console.log("here 1");
+    console.log(changes);
   }
 
   selectDate(): void {
     this.store.dispatch(changeSelectedDate({date: new Date(this.date["year"],this.date["month"],this.date["date"])}))
   }
 
-  ngOnDestroy(): void {
-    console.log("here");
+  test(){
+    console.log("here 2")
   }
-
 }
--- src/app/calendar/date-rows/date-rows.component.ts
+++ src/app/calendar/date-rows/date-rows.component.ts
@@ -7,60 +7,151 @@ import { Note } from '../../app.reducer';
 @Component({
   selector: 'app-date-rows',
   templateUrl: './date-rows.component.html',
-  styleUrls: ['./date-rows.component.css']
+  styleUrls: ['./date-rows.component.css'],
 })
 export class DateRowsComponent implements OnInit {
-
-  currentDate:Date = new Date();
-  month:number;
-  year:number;
-  dateList:Array<Object>;
-
-  constructor(private store:Store) { }
+  currentDate: Date = new Date();
+  month: number;
+  year: number;
+  dateColorList: Array<Object>;
+  notes: Array<Note> = [];
+  constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.store.pipe(select(getPreviewMonthAndYear)).subscribe(monthAndYear => {
-      this.month = monthAndYear[0];
-      this.year = monthAndYear[1];
+    console.log("here")
+    this.store
+      .pipe(select(getPreviewMonthAndYear))
+      .subscribe((monthAndYear) => {
+        console.log(1);
+        this.month = monthAndYear[0];
+        this.year = monthAndYear[1];
+        this.generateDateList();
+      });
+    this.store.pipe(select(getNotesByDesc)).subscribe((notes) => {
+      console.log(2);
+      this.notes = notes;
       this.generateDateList();
     });
   }
+  test(): void {
+    console.log(100);
+  }
+  generateDateList(): void {
+    let dateList: Array<Object> = [];
 
-  generateDateList(): void{
-    
-    this.dateList = []
-    
     // Create a date object with selected month and year
-    let firstDate:Date = new Date(this.year,this.month,1);
+    let firstDate: Date = new Date(this.year, this.month, 1);
     // Get last date
-    let lastDate:Date = new Date(this.year,(this.month+1)%12,0);
+    let lastDate: Date = new Date(this.year, (this.month + 1) % 12, 0);
     // Get previous month last date
-    let lastDatePreviousMonth:Date = new Date(this.year,this.month,0);
+    let lastDatePreviousMonth: Date = new Date(this.year, this.month, 0);
     // Get first date day
-    let firstDateDay:number  = firstDate.getDay();
+    let firstDateDay: number = firstDate.getDay();
     // Get next month first date
-    let nextMonthDate:Date = new Date(this.year, (this.month+1), 1);
+    let nextMonthDate: Date = new Date(this.year, this.month + 1, 1);
 
     // Calculate previous month dates to show
-    let previousMonthDates:Array<Object> = [];
-    for(let i = 0;i<firstDateDay;i++){
-      previousMonthDates.unshift({"date": lastDatePreviousMonth.getDate()-i,"month": lastDatePreviousMonth.getMonth(),"year": lastDatePreviousMonth.getFullYear()});
+    let previousMonthDates: Array<Object> = [];
+    for (let i = 0; i < firstDateDay; i++) {
+      previousMonthDates.unshift({
+        date: lastDatePreviousMonth.getDate() - i,
+        month: lastDatePreviousMonth.getMonth(),
+        year: lastDatePreviousMonth.getFullYear(),
+        colors: [],
+      });
     }
 
     // Get the current month dates to show
-    let currentMonthDates:Array<Object> = [];
-    for(let i=1;i<=lastDate.getDate();i++){
-      currentMonthDates.push({"date": i,"month": firstDate.getMonth(),"year": firstDate.getFullYear()});
+    let currentMonthDates: Array<Object> = [];
+    for (let i = 1; i <= lastDate.getDate(); i++) {
+      currentMonthDates.push({
+        date: i,
+        month: firstDate.getMonth(),
+        year: firstDate.getFullYear(),
+        colors: [],
+      });
     }
 
     // Get next month dates to show
-    let nextMonthDates:Array<Object> = [];
-    for(let i=1;i<=(42-(previousMonthDates.length+currentMonthDates.length));i++){
-      nextMonthDates.push({"date": i,"month": nextMonthDate.getMonth(),"year": nextMonthDate.getFullYear()});
+    let nextMonthDates: Array<Object> = [];
+    for (
+      let i = 1;
+      i <= 42 - (previousMonthDates.length + currentMonthDates.length);
+      i++
+    ) {
+      nextMonthDates.push({
+        date: i,
+        month: nextMonthDate.getMonth(),
+        year: nextMonthDate.getFullYear(),
+        colors: [],
+      });
     }
 
     // Combine all dates
-    this.dateList = this.dateList.concat(previousMonthDates,currentMonthDates,nextMonthDates);
+    dateList = dateList.concat(
+      previousMonthDates,
+      currentMonthDates,
+      nextMonthDates
+    );
 
-  }  
+    this.combineColorsByDate(dateList);
+  }
+
+  combineColorsByDate(dateList: Array<Object>): void {
+    let dateColorList = [];
+    let firstDateObject = new Date(
+      dateList[0]['year'],
+      dateList[0]['month'],
+      dateList[0]['date']
+    );
+    let lastDateObject = new Date(
+      dateList[41]['year'],
+      dateList[41]['month'],
+      dateList[41]['date']
+    );
+    let noteObjectByDate = {};
+    for (let note of this.notes) {
+      let lastModifiedDate = new Date(
+        note.lastModified.getFullYear(),
+        note.lastModified.getMonth(),
+        note.lastModified.getDate()
+      );
+      if (
+        lastModifiedDate >= firstDateObject &&
+        lastModifiedDate <= lastDateObject
+      ) {
+        let stringifiedDate =
+          lastModifiedDate.getFullYear().toString() +
+          '-' +
+          lastModifiedDate.getMonth().toString() +
+          '-' +
+          lastModifiedDate.getDate().toString();
+        if (stringifiedDate in noteObjectByDate) {
+          noteObjectByDate[stringifiedDate] = noteObjectByDate[
+            stringifiedDate
+          ].concat(note.color);
+        } else {
+          noteObjectByDate[stringifiedDate] = [];
+          noteObjectByDate[stringifiedDate] = noteObjectByDate[
+            stringifiedDate
+          ].concat(note.color);
+        }
+      }
+      if (lastModifiedDate < firstDateObject) break;
+    }
+    for (let dateObject of dateList) {
+      let stringifiedDate =
+        dateObject['year'] +
+        '-' +
+        dateObject['month'] +
+        '-' +
+        dateObject['date'];
+      if (stringifiedDate in noteObjectByDate) {
+        dateObject['colors'] = [...new Set(noteObjectByDate[stringifiedDate])];
+      }
+      dateColorList = dateColorList.concat(dateObject);
+    }
+    console.log(dateColorList);
+    this.dateColorList = dateColorList;
+  }
 }
